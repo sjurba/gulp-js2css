96d99fbe2e5a3954f6ff31ab783806e8806d7ce3
Sjur Bakka <sjurba@gmail.com>
2015-05-29 19:03:24 -0400

    Fixed tests

--- test.js
+++ test.js
@@ -4,20 +4,20 @@ var gutil = require('gulp-util');
 var js2css = require('./');
 
 it('should process buffer', function(cb) {
-    var stream = js2css();
+	var stream = js2css();
 
-    stream.on('data', function(file) {
-        assert.strictEqual(file.contents.toString(), 'body{\n\tcolor: red;\n}\n');
-        assert.strictEqual(file.path, __dirname + '/file.css');
-    });
+	stream.on('data', function(file) {
+		assert.strictEqual(file.contents.toString().replace(/\s*/g, ''), 'body{color:red;}');
+		assert.strictEqual(file.path, __dirname + '/file.css');
+	});
 
-    stream.on('end', cb);
+	stream.on('end', cb);
 
-    stream.write(new gutil.File({
-        base: __dirname,
-        path: __dirname + '/file.css.js',
-        contents: new Buffer('module.exports = {body : {color: "red"}};')
-    }));
+	stream.write(new gutil.File({
+		base: __dirname,
+		path: __dirname + '/file.css.js',
+		contents: new Buffer('module.exports = {body : {color: "red"}};')
+	}));
 
-    stream.end();
+	stream.end();
 });
